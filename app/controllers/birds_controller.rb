68befd3e11d3188392888892256757e7bb599367
seenso <>
2022-01-11 09:51:22 -0900

    commit tree 870452b56a3b7147d2070227674f6030d4000422

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -44,6 +44,20 @@ class BirdsController < ApplicationController
     end
   end
 
+  # DELTE /birds/:id
+  def destroy 
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      bird.destroy
+      head :no_content #returns 204 "no content" status in header
+      # OR we can render an empty json object like...
+      # render json: {}
+      #whichever option you prefer is basd on how you'll use the data in the client application
+    else
+      render json: { error: "Bird not found" }, status: :not_found
+    end
+  end
+
   private
 
   def bird_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  resources :birds, only: [:index, :show, :create, :update]
+  # resources :birds, only: [:index, :show, :create, :update, :destroy]
+  #using all 5 RESTful routes so we can get rid of the only option
+  resources :birds
   patch "/birds/:id/like", to: "birds#increment_likes"
 end
